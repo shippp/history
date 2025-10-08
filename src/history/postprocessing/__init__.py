5617a77cc85a90b52ae8934cca451e6921bb77a1
godinlu <godin.luc2002@gmail.com>
2025-10-08 17:04:54 +0200

    add all plots without outliers

--- src/history/postprocessing/__init__.py
+++ src/history/postprocessing/__init__.py
@@ -4,8 +4,7 @@ from .core import PostProcessing
 # from .coregistration import coregister_dem, iter_coregister_dems
 from .io import *
 from .plotting import *
-from .visualization import PostProcessPlot
 
 # from .point2dem import iter_point2dem, point2dem
 
-__all__ = ["visualization", "PostProcessing", "PostProcessPlot"]
+__all__ = ["visualization", "PostProcessing"]
